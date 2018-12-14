c6998ba783d5bc92c329e965dec5805a4543ac18
Olav Vatne <olavvatne@gmail.com>
2018-12-14 16:11:03 +0100

    Adds name, constant velocity and effort

--- rbx1_slush/scripts/joint_states_publisher.py
+++ rbx1_slush/scripts/joint_states_publisher.py
@@ -14,9 +14,12 @@ def joint_state_publisher():
         msg=JointState()
         msg.header.stamp = rospy.Time.now()
         status = robot.getStatus()
+        msg.name = ['gripper_idol_gear_joint', 'gripper_servo_gear_joint',
+         'joint_1_base_shoulder', 'joint_2_shoulder_arm', 'joint_3_arm_upper_forearm', 
+         'joint_4_upper_forearm_forearm', 'joint_5_forearm_wrist', 'joint_6_wrist_hand']
         msg.position = status['currentPos']
-        msg.effort = [0, 0, 0, 0, 0, 0]
-        msg.velocity = [0, 0, 0, 0, 0, 0]
+        msg.effort = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+        msg.velocity = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
         rospy.loginfo(msg)
         pub.publish(msg)
         rate.sleep()
