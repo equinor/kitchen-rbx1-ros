61f006b67e78fe47bfbe0fca4780b63bb066f02e
Olav Vatne <olavvatne@gmail.com>
2019-01-04 16:22:46 +0100

    Remove axis negate. Must adjust orientation in unity

--- rbx1_slush/src/rbx1/robot.py
+++ rbx1_slush/src/rbx1/robot.py
@@ -12,11 +12,11 @@ class Robot:
         # steps per revolution obtained by manual obervation of steps to do 90 degree
         # multiplying by four to get steps per 360 degree.
         self._axis = [
-            Axis(Slush.Motor(1), minimum=-8000, maximum=5500, speed=10, current=[65, 65, 65, 65], steps=32000, negate=True), #Shoulder
+            Axis(Slush.Motor(1), minimum=-8000, maximum=5500, speed=10, current=[65, 65, 65, 65], steps=32000), #Shoulder
             Axis(Slush.Motor(0), minimum=-4000, maximum=4000, speed=10, current=[65, 70, 60, 70], steps=16000), #Arm
             Axis(Slush.Motor(2), minimum=-20000, maximum=20000, speed=30, current=[50, 50, 50, 50], steps=64000),
             Axis(Slush.Motor(3), minimum=-3000, maximum=3000, speed=20, current=[75, 75, 75, 75], steps=6000),
-            Axis(Slush.Motor(4), minimum=-4000, maximum=4000, speed=20, current=[85, 85, 85, 85], steps=14000, negate=True),
+            Axis(Slush.Motor(4), minimum=-4000, maximum=4000, speed=20, current=[85, 85, 85, 85], steps=14000),
             Axis(Slush.Motor(5), minimum=-1650, maximum =1650, speed=20, current=[65,65, 65, 65], steps=3600)
         ]
         self._gripper = Gripper()
